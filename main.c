0a30a71af76242d80041b70f44716de26be310bb
Thomas <thomas@geenbs.nl>
2017-10-31 15:43:51 +0100

    More test code for irc base

--- main.c
+++ main.c
@@ -5,20 +5,35 @@
 #include "tcp_client.h"
 #include "irclib.h"
 
+void freeLinkedList(chanList *targetList){
+    if(targetList->next == NULL){
+        free(targetList);
+    }else{
+        chanList *head = targetList;
+        chanList *curr;
+        while ((curr = head) != NULL) { // set curr to head, stop if list empty.
+            head = head->next;          // advance head to next element.
+            free (curr);                // delete saved pointer.
+        }
+    }
+    printf("Done freeing targetList\n");
+}
+
 int main(void){
 
     //Connect to server
-    //char serverName[] = "geenbs.nl";
+    //char serverName[] = "germany.enterthegame.com";
     //int serverPort =  6665;
-    char serverName[] = "irc.freenode.org";
-    int serverPort =  6667;
+    char serverName[] = "geenbs.nl";
+    int serverPort =  6665;
+    //char serverName[] = "irc.freenode.org";
+    //int serverPort =  6667;
     int clientSocket = connectToServer(serverName, serverPort, 12);
 
     if(clientSocket == 0){
         printf("Connect failed with code %d\n", clientSocket);
         return 1;
     }
-    printf("Socket has nr %d\n", clientSocket);
 
     //Populate ircData struct
     ircData myData;
@@ -27,6 +42,7 @@ int main(void){
 
     int rc = ircLogin(&myData, &clientSocket);
 
+    printf("irclogin rc = %d\n", rc);
     if(rc > 0){
         printf("Error logging in, rc %d\n", rc);
         return 1;
@@ -35,15 +51,23 @@ int main(void){
     //test channels linked list, todo: offload this to external file
     chanList *chans = malloc(sizeof(chanList));
 
-    getAllChannels(&clientSocket, chans);
+    if(getAllChannels(&clientSocket, chans) == -2){
+        printf("Recall getallchans..\n");
+        getAllChannels(&clientSocket, chans);
+    }
 
-    joinChannels(&clientSocket, chans);
+    rc = joinChannels(&clientSocket, chans);
 
     parseResponses(&clientSocket);
     
     //spawnShell(&clientSocket);
 
     close(clientSocket);
+
+    if(chans->next != NULL)
+        freeLinkedList(chans);
+    else
+        free(chans);
 
     return 0;
 }
