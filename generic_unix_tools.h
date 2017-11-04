40580fbec28df47df43f2fc4216747ec85aa74c5
Thomas <thomas@geenbs.nl>
2017-11-04 13:27:39 +0100

    Initial commit

---
+++ generic_unix_tools.h
@@ -0,0 +1,10 @@
+#ifndef generic_unix_tools_H_
+#define generic_unix_tools_H_
+
+//Count nr of lines in file
+extern int countLines(char *fileName);
+
+/* Returns true or false if regex matches string
+Usage: regexMatch("thomas$", "this is a test string mr thomas");*/
+extern int regexMatch(char *regex, char *string);
+#endif
