42db4707e63c700908b0432b8bc0004d2d09b67f
Thomas <thomas@geenbs.nl>
2017-11-14 18:08:39 +0100

    Using newer version

--- generic_unix_tools.h
+++ generic_unix_tools.h
@@ -1,10 +1,21 @@
 #ifndef generic_unix_tools_H_
 #define generic_unix_tools_H_
 
+/*A generic placeholder linked list, used by
+ * freeLinkedListGen()*/
+typedef struct agenericLinkedList{
+    //int a;
+    char chanName[100];
+    struct agenericLinkedList *next;
+}genL;
+
 //Count nr of lines in file
 extern int countLines(char *fileName);
 
 /* Returns true or false if regex matches string
 Usage: regexMatch("thomas$", "this is a test string mr thomas");*/
 extern int regexMatch(char *regex, char *string);
+
+//Generic linked list free
+extern void freeLinkedListGen(void* targetList);
 #endif
