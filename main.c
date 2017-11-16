5f0616a712c4bd4d1b24c5ffb3b43f23e2292820
Thomas <thomas@geenbs.nl>
2017-11-16 10:48:41 +0100

    Removed free linked list funtion

--- main.c
+++ main.c
@@ -7,16 +7,9 @@
 #include "irclib.h"
 #include "generic_unix_tools.h"
 
-extern void freeLinkedList(chanList *targetList);
-
 int main(void){
     printf("Retrieving automated responses...\n");
     
-    //Parameterize this filename
-    int lineCnt = countLines("replies.txt");
-    aR replies[lineCnt + 1];
-    retrieveAutomatedReplies(replies, "replies.txt");
-
     //Retreive configuration parameters
     appConfig config;
     if(getConfig(&config, "config.txt") == 1){ //Offload this later as parameter
@@ -24,13 +17,13 @@ int main(void){
         return 1;
     }
 
+    //Connect to server and retrieve socket
     int clientSocket = connectToServer(config.serverName, config.serverPort, 12);
 
     if(clientSocket == 0){
         printf("Connect failed with code %d\n", clientSocket);
         return 1;
     }
-
     int rc = ircLogin(&config, &clientSocket);
 
     printf("irclogin rc = %d\n", rc);
@@ -39,8 +32,14 @@ int main(void){
         return 1;
     }
     
+    //todo: Parameterize this filename
+    int lineCnt = countLines("replies.txt");
+    aR replies[lineCnt + 1];
+    retrieveAutomatedReplies(replies, "replies.txt");
+    
     //test channels linked list, todo: offload this to external file
     chanList *chans = malloc(sizeof(chanList));
+    chans->next = NULL;
 
     printf("\nRequesting all channels...\n");
     if(getAllChannels(&clientSocket, chans) == -2){
@@ -50,35 +49,17 @@ int main(void){
     if(rc == 0)
         rc = joinChannels(&clientSocket, chans);
 
+    //Running parsing of responses
     printf("Starting parseResponses..\n");
     parseResponses(&clientSocket, replies);
     
+    //Spawn "shell", mainly for debugging
     //spawnShell(&clientSocket);
 
     close(clientSocket);
 
-    if(chans->next != NULL)
-        freeLinkedList(chans);
-    else
-        free(chans);
-
-    //Dont forget to free replies
+    freeChannels(chans);
 
     return 0;
 }
 
-//Free a linked list
-extern void freeLinkedList(chanList *targetList){
-    
-    if(targetList->next == NULL){
-        free(targetList);
-    }else{
-        chanList *head = targetList;
-        chanList *curr;
-        while ((curr = head) != NULL) { // set curr to head, stop if list empty.
-            head = head->next;          // advance head to next element.
-            free (curr);                // delete saved pointer.
-        }
-    }
-    printf("Done freeing targetList\n");
-}
